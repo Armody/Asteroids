4a72cbd99e1e584745ce8bbfc92db62f1812eddf
Armody <Rmdyzie@gmail.com>
2024-09-02 21:06:32 +0300

    new file:   circleshape.py 	modified:   constants.py 	modified:   main.py 	new file:   player.py

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    framerate = pygame.time.Clock()
+    clock = pygame.time.Clock()
     dt = 0
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
 
     while True:
         for event in pygame.event.get():
@@ -13,10 +17,10 @@ def main():
                 return
         
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
 
-        framerate.tick(60)
-        dt = framerate.tick(60) / 1000
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
