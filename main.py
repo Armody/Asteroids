0cdd9bf1035a0609fc8b7b9855e512bc830ae146
Armody <Rmdyzie@gmail.com>
2024-09-02 20:27:40 +0300

    modified:   main.py

--- main.py
+++ main.py
@@ -3,14 +3,21 @@ from constants import *
 
 def main():
     pygame.init()
-    print("Starting asteroids!")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    framerate = pygame.time.Clock()
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        pygame.Surface.fill(screen, (0, 0, 0))
+        
+        screen.fill("black")
         pygame.display.flip()
+
+        framerate.tick(60)
+        dt = framerate.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
